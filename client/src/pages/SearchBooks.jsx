502b46469b7183bb7d95c5eb701e4b59bfe03c5b
Paul Sullivan <paul8sullivan@gmail.com>
2024-01-31 06:26:55 -0500

    debugging

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -10,6 +10,8 @@ import {
   Row
 } from 'react-bootstrap';
 
+import { searchGoogleBooks } from '../utils/API';
+
 import Auth from '../utils/auth';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
--- server/server.js
+++ server/server.js
@@ -4,16 +4,17 @@ const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
+// not needed for GraphQL API
+// const routes = require('./routes');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
-
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -23,6 +24,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
